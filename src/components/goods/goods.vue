41a3222c1cf5271187e1dca281e53ffa663f61ba
Jason <jiawei9402759@163.com>
2019-05-31 20:49:26 +0800

    update

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,9 +1,125 @@
 <template>
-    <div class="goods">goodsa</div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" :key="item.name" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" :key="item.name" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" :key="food.name" class="food-item border-1px">
+              <div class="icon">
+                <img :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span>月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span>￥{{food.price}}</span>
+                  <span v-if="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+  const ERR_OK = 0
   export default {
-    name: 'goods'
+    name: 'goods',
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: []
+      }
+    },
+    created () {
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.goods = response.data
+        }
+      })
+      this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+    }
   }
 </script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .goods
+    display flex
+    position absolute
+    width 100%
+    top 174px
+    bottom 46px
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background: #f3f5f7
+      .menu-item
+        display table
+        padding 0 12px
+        height 54px
+        width 56px
+        line-height 14px
+        .icon
+          display inline-block
+          vertical-align top
+          width 12px
+          height 12px
+          margin-right 2px
+          background-size 12px 12px
+          background-repeat no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display table-cell
+          width 56px
+          vertical-align middle
+          border-1px-after(rgba(7,17,27,0.1))
+          font-size 12px
+    .foods-wrapper
+      flex 1
+      .title
+        padding-left 14px
+        height 26px
+        line-height 26px
+        border-left 2px solid #d9dde1
+        font-size 12px
+        color rgb(147,153,159)
+        background #f3f5f7
+      .food-item
+        display flex
+        margin 18px
+        border-1px-after(rgba(7,17,27,0.1))
+</style>
