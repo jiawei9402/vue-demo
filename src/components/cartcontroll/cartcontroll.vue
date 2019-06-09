8067906f1e978d45954a7e68f71683e498e61bc3
jason <jiawei9402759@163.com>
2019-06-09 17:01:29 +0800

    cartcontroll

---
+++ src/components/cartcontroll/cartcontroll.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class='cartcontroll'>
+      <transition name="fade">
+        <div class="cart-decrease" v-if="food.count > 0" @click="decreaseFood">
+          <span class="inner icon-remvoe_circle_outline"></span>
+        </div>
+      </transition>
+      <div class="cart-count" v-show="food.count > 0">{{food.count}}</div>
+      <div class="cart-add icon-add_circle" @click="addFood"></div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Vue from 'vue'
+  export default {
+    name: 'cartcontroll',
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    methods: {
+      addFood() {
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1)
+        } else {
+          this.food.count++
+        }
+      },
+      decreaseFood() {
+        if (this.food.count > 0) {
+          this.food.count--
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .cartcontroll
+    font-size 0
+    .fade-enter-active,.fade-leave-active
+      transition all .5s linear
+    .fade-enter-to, .fade-leave /* .fade-leave-active below version 2.1.8 */
+      opacity 1
+      transform translate3D(0,0,0)
+      .inner
+        transform rotate(180deg)
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+      opacity 0
+      transform translate3D(24px,0,0)
+      .inner
+        transform rotate(0)
+    .cart-decrease
+      display inline-block
+      padding 6
+      .inner
+        line-height 24px
+        font-size 24px
+        color rgb(0,160,220)
+    .cart-count
+      display inline-block
+      line-height 24px
+      vertical-align top
+      width 12px
+      padding-top 6px
+      text-align center
+      font-size 10px
+      color rgb(147,153,159)
+    .cart-add
+      display inline-block
+      padding 6px
+      line-height 24px
+      font-size 24px
+      color rgb(0,160,220)
+</style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,8 @@
   <div class="goods">
     <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" :key="item.name" class="menu-item">
+        <li v-for="(item, index) in goods" :key="index" class="menu-item" @click="selectMenu(index, $event)"
+            :class="{'current' : currentIndex === index}">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -12,7 +13,7 @@
     </div>
     <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" :key="item.name" class="food-list">
+        <li v-for="item in goods" :key="item.name" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" :key="food.name" class="food-item border-1px">
@@ -27,7 +28,11 @@
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span><span class="old" v-if="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span class="old"
+                        v-if="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+                <div class="cartcontroll-wrapper">
+                  <cartcontroll :food="food"></cartcontroll>
                 </div>
               </div>
             </li>
@@ -35,11 +40,15 @@
         </li>
       </ul>
     </div>
+    <shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   import BScroll from 'better-scroll'
+  import shopcart from '../shopcart/shopcart'
+  import cartcontroll from '../cartcontroll/cartcontroll'
+
   const ERR_OK = 0
   export default {
     name: 'goods',
@@ -48,31 +57,84 @@
         type: Object
       }
     },
-    data () {
+    data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       }
     },
-    created () {
+    created() {
       this.$http.get('/api/goods').then((response) => {
+        this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
         response = response.body
         if (response.errno === ERR_OK) {
           this.goods = response.data
           this.$nextTick(() => {
             this._initScroll()
+            this._calculateHeight()
           })
         }
       })
-      this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i]
+          let height2 = this.listHeight[i + 1]
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i
+          }
+        }
+        return 0
+      },
+      selectFoods() {
+        let foods = []
+        this.goods.forEach((good) => {
+          good.foods.forEach((food) => {
+            if (food.count && food.count > 0) {
+              foods.push(food)
+            }
+          })
+        })
+        return foods
+      }
     },
     methods: {
+      selectMenu(index, event) {
+        if (!event._constructed) {
+          return
+        }
+        let foodsList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+        let el = foodsList[index]
+        this.foodsScroll.scrollToElement(el, 300)
+      },
       _initScroll() {
-        this.menuScroll = new BScroll(this.$refs.menuWrapper, {})
-        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {})
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true
+        })
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          probeType: 3,
+          click: true
+        })
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y))
+        })
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < foodList.length; i++) {
+          height += foodList[i].clientHeight
+          this.listHeight.push(height)
+        }
       }
     },
     components: {
-      BScroll
+      BScroll,
+      shopcart,
+      cartcontroll
     }
   }
 </script>
@@ -86,16 +148,29 @@
     top 174px
     bottom 46px
     overflow hidden
+
     .menu-wrapper
       flex 0 0 80px
       width 80px
       background: #f3f5f7
+
       .menu-item
         display table
         padding 0 12px
         height 54px
         width 56px
         line-height 14px
+
+        &.current
+          position: relative
+          z-index 10
+          margin-top -1px
+          background #fff
+          font-weight 700
+
+          .text
+            border-none()
+
         .icon
           display inline-block
           vertical-align top
@@ -104,69 +179,94 @@
           margin-right 2px
           background-size 12px 12px
           background-repeat no-repeat
+
           &.decrease
             bg-image('decrease_3')
+
           &.discount
             bg-image('discount_3')
+
           &.guarantee
             bg-image('guarantee_3')
+
           &.invoice
             bg-image('invoice_3')
+
           &.special
             bg-image('special_3')
+
         .text
           display table-cell
           width 56px
           vertical-align middle
-          border-1px-after(rgba(7,17,27,0.1))
+          border-1px-after(rgba(7, 17, 27, 0.1))
           font-size 12px
+
     .foods-wrapper
       flex 1
+
       .title
         padding-left 14px
         height 26px
         line-height 26px
         border-left 2px solid #d9dde1
         font-size 12px
-        color rgb(147,153,159)
+        color rgb(147, 153, 159)
         background #f3f5f7
+
       .food-item
         display flex
         margin 18px
         padding-bottom 18px
-        border-1px-after(rgba(7,17,27,0.1))
+        border-1px-after(rgba(7, 17, 27, 0.1))
+
         &:last-child
           border-none()
           margin-bottom 0px
+
         .icon
           flex 0 0 57px
           margin-right 10px
+
         .content
           flex 1
+
           .name
             margin 2px 0 8px 0
             height 14px
             line-height 14px
             font-size 14px
-            color rgb(7,17,27)
+            color rgb(7, 17, 27)
+
           .desc, .extra
-            line-height 10px
+            line-height 14px
             font-size 10px
-            color rgb(147,153,159)
+            color rgb(147, 153, 159)
+
           .desc
             margin-bottom 8px
+            line-height 12px
+
           .extra
             &.count
               margin-right 12px
+
           .price
             font-weight 700
             line-height 14px
+
             .now
               margin-right 8px
               font-size 14px
-              color rgb(240,20,20)
+              color rgb(240, 20, 20)
+
             .old
               text-decoration line-through
               font-size 10px
-              color rgb(147,153,159)
+              color rgb(147, 153, 159)
+
+          .cartcontroll-wrapper
+            position absolute
+            right 0
+            bottom 12px
 </style>
